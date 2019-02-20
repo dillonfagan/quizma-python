406f90c65fb442968ee502645c97864129ef0317
Dillon Fagan <dillonfagan@gmail.com>
2019-02-19 19:04:17 -0500

    Begin to add widgets to app window

--- src/app.py
+++ src/app.py
@@ -7,6 +7,20 @@ class Application(Frame):
         master.title('Quizma')
         master.geometry('300x200')
         self.pack()
+        self.create_widgets()
+    
+    def create_widgets(self):
+        self.question_label_text = StringVar()
+        self.question_label_text.set('Is this a question?')
+
+        self.question_label = Label(self, textvariable=self.question_label_text)
+        self.question_label.pack()
+
+        self.proceed_button_text = StringVar()
+        self.proceed_button_text.set('Next')
+
+        self.proceed_button = Button(self, textvariable=self.proceed_button_text)
+        self.proceed_button.pack()
 
 
 app = Application()
